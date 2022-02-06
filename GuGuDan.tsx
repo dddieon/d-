8a821644f327daec6fc614501a9ec8dfe51aca44
dddieon <ajiop0123@gmail.com>
2022-02-07 03:21:19 +0900

    learn✨-1: 이벤트 핸들러, useRef 타이핑
    - 이벤트 핸들러의 e 객체타입은 ide에서 추론해준다 (검색 안해도 됨!)
    - useRef는 null이 초기값이므로, useRef 파라미터 타입에 <HTMLInputElement>를 넣었다
        [function useRef<T>(initialValue: T|null): RefObject<T>; 로 되어있는 useRef. 함수를 호출할 때, 함수 안에서 사용할 타입을 넣어줄 수 있다]

---
+++ GuGuDan.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { useRef, useState } from 'react';
+import { setConstantValue } from 'typescript';
+
+function GuGuDan() {
+    const [first, setFirst] = useState(Math.ceil(Math.random() * 9)) // 1에서 9까지 랜덤
+    const [second, setSecond] = useState(Math.ceil(Math.random() * 9)) // 1에서 9까지 랜덤
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputEl = useRef<HTMLInputElement>(null); // input을 null이 아니라고 ---- 1
+
+    const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => { // e 위에 커서올리면 ide에서 타입추론 해준다
+        e.preventDefault();
+        const input = inputEl.current;
+        if (parseInt(value) === first * second) {
+            setResult('정답');
+            setFirst(Math.ceil(Math.random() * 9));
+            setSecond(Math.ceil(Math.random() * 9));
+            setValue('');
+            input?.focus(); // input이 null이 아니라고 --- 2
+        } else {
+            setResult('땡');
+            input?.focus();
+        }
+    }
+
+    return (
+        <>
+            <div>{first} 곱하기 {second}는?</div>
+            <form onSubmit={onSubmitForm}>
+                <input 
+                    ref={inputEl} 
+                    type="text" 
+                    value={value} 
+                    onChange={e => setValue(e.target.value)}/>
+            </form>
+            <div>{result}</div>
+        </>
+    )
+}
+
+export default GuGuDan;
