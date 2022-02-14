6aedc7a0164c880d11aed6a6075d41de5383e039
dddieon <ajiop0123@gmail.com>
2022-02-15 01:45:04 +0900

    env🌱: webpack-dev-server setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: "./index.html",
-    filename: "./index.html"
- });
+  template: './public/index.html',
+  filename: './index.html',
+});
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval', // production : hidden-source-map
-    resolve: {
-        extensions: ['.jsx', '.js', '.tsx', '.ts']
-    },
-    entry: {
-        app: './client'
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: 'ts-loader'
-        }]
-    },
-    plugins: [
-        htmlPlugin //webpack-dev-server 용 index.html 로더
+  mode: 'development',
+  devtool: 'eval', // production : hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
     ],
-    output: {
-        filename: '[name].js',
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/dist' //webpack-dev-server 용
-    }
-}
+  },
+  plugins: [
+    htmlPlugin, //webpack-dev-server 용 index.html 로더
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist', //webpack-dev-server 용
+  },
+};
